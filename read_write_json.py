3dacbf2f6ad8c4eb09f68f8bf5f17390e7192b3d
Jessica <jkelly@urban.org>
2017-07-20 10:50:22 -0400

    :star: :snake: python for reading, updating and writing json

---
+++ read_write_json.py
@@ -0,0 +1,31 @@
+# Jessica A Kelly
+#
+# Just a simple program that reads a JSON file, does a transformation on a
+# variable and writes back a new item to the JSON file
+#
+# 20 July 2017
+
+import json
+import pprint
+
+# Read JSON file
+with open('ex_form_json.json') as data_file:
+    jdata = json.load(data_file)
+
+pprint.pprint(jdata) #print it out "pretty"
+
+print("count is ",jdata["count"])
+print("getting at one single param name is ", jdata["data"][0]["PARAM"])
+print("getting at one single param value is ", jdata["data"][0]["VAL"])
+
+# transform and write back to the same element?
+# easy, just change the value and then write the file.
+jdata["data"][0]["VAL"] = 51
+# Writing JSON data
+with open('ex_form_json_updated.json', 'w') as f:
+     json.dump(jdata, f)
+
+# or transform and write to a new element?
+jdata["data"][0]["NEWVAL"] = 51
+with open('ex_form_json_new.json', 'w') as f:
+    json.dump(jdata,f)
